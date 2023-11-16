8a6a07200226968820212c67a1d737c6a711fb8a
DongHun Park <68440583+Laeti-Park@users.noreply.github.com>
2023-11-16 18:09:05 +0900

    feat: 스크립트 파일 추가

--- start.bat
+++ start.bat
@@ -6,5 +6,8 @@ START /wait /b cmd /c npm install
 :: 데이터베이스 테이블 생성
 START /wait /b cmd /c npm run migration:run
 
+:: Seed 데이터 삽입
+START /wait /b cmd /c npm run seed:run
+
 :: NestJS 앱 실행
 npm run start:local
--- start.sh
+++ start.sh
@@ -4,5 +4,8 @@ npm install
 # 데이터베이스 테이블 생성
 npm run migration:run
 
+# Seed 데이터 삽입
+npm run seed:run
+
 # NestJS 앱 실행
 npm run start:local
